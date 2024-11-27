242b217c50a21c5e1339baea6e554dbc75741d8c
rago7 <gopi.edu7@gmail.com>
2024-11-26 22:59:31 -0500

    created initial schema for users, posts, likes, comments, chats, notifications

--- srccode/be/backend/backend/settings.py
+++ srccode/be/backend/backend/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'theapp_db',       # Your database name
+        'USER': 'postgres',     # Your database username
+        'PASSWORD': 'theapp',# Your database password
+        'HOST': 'localhost',        # Use 127.0.0.1 for local setup
+        'PORT': '5432',             # Default PostgreSQL port
     }
 }
 
